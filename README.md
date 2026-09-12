b58bac00a0acd11d1b67f917622c9a6e2d2c51ff
KyleBuii <buikyle12@gmail.com>
2026-09-12 11:32:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zominerary
+Call of Duty zombies easter egg helper (currently only Black ops 7)
---
+++ src/Homepage.jsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+import './Homepage.scss';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>Zominerary</div>
+    );
+};
+
+export default memo(Homepage);
